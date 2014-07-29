52a891abfe59411ba26056cf5e4583f25e3c1f52
velo.yamigiku <velo.yamigiku@gmail.com>
2014-07-29 10:34:38 +0900

    コメントを修正しました。

--- GooglePlaySample/src/main/java/jp/co/myself/googleplaysample/HelloAndroidActivity.java
+++ GooglePlaySample/src/main/java/jp/co/myself/googleplaysample/HelloAndroidActivity.java
@@ -11,16 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class HelloAndroidActivity extends Activity {
-
-	/**
-	 * Called when the activity is first created.
-	 * 
-	 * @param savedInstanceState
-	 *            If the activity is being re-initialized after previously being
-	 *            shut down then this Bundle contains the data it most recently
-	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
-	 *            is null.</b>
-	 */
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
