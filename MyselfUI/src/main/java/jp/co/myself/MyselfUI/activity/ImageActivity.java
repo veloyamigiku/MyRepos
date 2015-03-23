a6d538899afce5522c3f5a46d65eaf9f44b10273
velo.yamigiku <velo.yamigiku@gmail.com>
2015-03-24 02:58:50 +0900

    MS0004 画像ビューア(単体)を追加します。

--- MyselfUI/src/main/java/jp/co/myself/MyselfUI/activity/ImageActivity.java
+++ MyselfUI/src/main/java/jp/co/myself/MyselfUI/activity/ImageActivity.java
@@ -1,12 +1,12 @@
 package jp.co.myself.MyselfUI.activity;
 
 import jp.co.myself.MyselfUI.R;
+import uk.co.senab.photoview.PhotoView;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 /**
@@ -15,7 +15,7 @@ import android.widget.Toast;
 public class ImageActivity extends Activity {
 	
 	/**
-	 * 
+	 * 画像データ受け取るためのインテントのキーです。
 	 */
 	public static final String INTENT_KEY_DATA = "DATA";
 	
@@ -27,19 +27,19 @@ public class ImageActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_image);
+		setContentView(R.layout.myselfui_activity_image);
 		
 		Intent intent = getIntent();
 		setImageData(intent.getByteArrayExtra(INTENT_KEY_DATA));
 		
-		ImageView iv = (ImageView) findViewById(R.id.imageView1);
+		PhotoView pv = (PhotoView) findViewById(R.id.myselfui_image_iv);
 		Bitmap bmp = null;
 		byte[] data = getImageData();
 		if (data != null && data.length > 0) {
 			bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
-			iv.setImageBitmap(bmp);
+			pv.setImageBitmap(bmp);
 		} else {
-			Toast.makeText(getApplicationContext(), "画像表示エラー", Toast.LENGTH_LONG);
+			Toast.makeText(getApplicationContext(), "画像表示エラー", Toast.LENGTH_LONG).show();
 		}
 	}
 	
